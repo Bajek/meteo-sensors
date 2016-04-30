652929dad285a25fbce43875156e7979854b892a
Bajek <bartlomiej.tomasz.urbaniec@cern.ch>
2016-05-01 01:15:38 +0200

    Minor

--- DS18B20/DS18B20.ino
+++ DS18B20/DS18B20.ino
@@ -52,6 +52,8 @@ void readAndSend() {
   dtostrf(temperature, 7, 3, charBuf);
 
   //For time being GET as it's easier.
+  //Issue #1
+  
   HTTPClient http;
   http.begin(String(url) + "?temp=" + String(charBuf) + "&vcc=" + String(ESP.getVcc()) + "&vdd=" + String(readvdd33())); //HTTP
   int httpCode = http.GET();
